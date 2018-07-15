3d67d7fe0e7828169319d89ef53cf93354337ec8
Cristian Stügelmayer <cristian@yellowspot.com.ar>
2018-07-15 16:22:13 -0300

    Promp always on new line

--- zsh-git-prompt.plugin.zsh
+++ zsh-git-prompt.plugin.zsh
@@ -1,3 +1,7 @@
 source ~/.oh-my-zsh/custom/plugins/zsh-git-prompt/zshrc.sh
-# an example prompt
-PROMPT='%B%m@%{$fg[blue]%}$(rvm-prompt)%{$fg[white]%}%~%b$(git_super_status) %# '
+
+# One line
+#PROMPT='%B%m@%{$fg[blue]%}$(rvm-prompt)%{$fg[white]%}%~%b$(git_super_status) %# '
+
+# Commands on new line
+PROMPT='%B%m@%{$fg[blue]%}$(rvm-prompt)%{$fg[white]%}%~%b$(git_super_status)'$'\n''() -> '
