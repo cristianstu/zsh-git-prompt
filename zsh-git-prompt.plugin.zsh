ef88e48f00ccf90c879a2fabc744d3dca4ad54ac
Cristian Stügelmayer <cristian@yellowspot.com.ar>
2018-08-05 19:56:51 -0300

    Show node version

--- zsh-git-prompt.plugin.zsh
+++ zsh-git-prompt.plugin.zsh
@@ -3,5 +3,18 @@ source ~/.oh-my-zsh/custom/plugins/zsh-git-prompt/zshrc.sh
 # One line
 #PROMPT='%B%m@%{$fg[blue]%}$(rvm-prompt)%{$fg[white]%}%~%b$(git_super_status) %# '
 
+node_version() {
+ echo "$fg[white]|$fg[green]node-$(nvm_prompt_info)"
+}
+
+# Ruby version and gemset
+# Git branch and state
 # Commands on new line
-PROMPT='%B%m@%{$fg[blue]%}$(rvm-prompt)%{$fg[white]%}%~%b$(git_super_status)'$'\n''() -> '
+#PROMPT='%B%m@%{$fg[blue]%}$(rvm-prompt)$(nvm_prompt_info)%{$fg[white]%}%~%b$(git_super_status)'$'\n''() -> '
+
+# Ruby version and gemset
+# Node version
+# Git branch and state
+# Commands on new line
+PROMPT='%B%m@%{$fg[blue]%}$(rvm-prompt)$(node_version)%{$fg[white]%}%~%b$(git_super_status)'$'\n''() -> '
+
